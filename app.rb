c9ddef987cd23df0dee8cd1757febf6f75604726
Jenae Christensen <729jenae.christensen@gmail.com>
2020-07-08 20:51:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,8 +11,8 @@ class App < Sinatra::Base
     erb :index 
   end 
   
-  post '/checkout' do 
-    session["item"] = params[:item]
-    @session = session 
-  end 
+  # post '/checkout' do 
+  #   session["item"] = params[:item]
+  #   @session = session 
+  # end 
 end
